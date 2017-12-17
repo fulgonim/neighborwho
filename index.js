6126652348ea980258f1d8977a1fd5c1cd79ad6c
Max Fulgoni <fulgonim@gmail.com>
2017-12-16 22:19:40 -0500

    Basic 311 call works, now refining with more specific query parameters

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const GOOGLE_MAP_API_KEY = "AIzaSyAPo7XvP2AAvQOk5i6WiJYW0htAG408Wxg";
 const GOOGLE_GEOCODE_API_URL = "https://maps.googleapis.com/maps/api/geocode/json";
-const GOOGLE_MAP_URL = ""
+const NYC_311_API_ENDPOINT_URL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json";
+const APP_TOKEN = "cWH2iJxbfljPUicrdhnNB7Znk";
+const SECRET_APP_TOKEN = "qdLc7ATCQe_VeoZ0omeXvnGN6pfXK5xF-IDu"
 
 
 function initWelcomeLightBox() {
@@ -46,7 +48,7 @@ let currentAddressInfo;
 
 
 // make sure to re-configure this function
-function addMarkerWithLocationData(coords, map) {
+function addMarkerWithLocationData(coords, map, locationData) {
     	console.log("this is the location data stored in the variable currentAddressInfo");
     	console.log(coords);
 
@@ -58,7 +60,7 @@ function addMarkerWithLocationData(coords, map) {
 		let infoWindow = new google.maps.InfoWindow({
 		content: `<h1>MY MARKER</h1>
 				<form class="js-map-popout-form">
-					<button class="js-map-find-out-more" type="submit">Find Out More</button>
+					<button onclick="nyc311Call(event)" class="js-map-find-out-more" type="submit">Find Out More</button>
 				</form>
 		`
 		});
@@ -66,6 +68,8 @@ function addMarkerWithLocationData(coords, map) {
 		marker.addListener('click', function() {
 			infoWindow.open(map, marker);
 		})
+
+		map.panTo(marker.getPosition());
 }
 
 // listen for search input and retrieve location data
@@ -99,23 +103,96 @@ function requestLocationDataWithAddress(getAddress) {
 		console.log(currentAddressInfo);
 
 		//create new marker with location info
-		addMarkerWithLocationData(currentAddressInfo.results[0].geometry.location, map);
+		addMarkerWithLocationData(currentAddressInfo.results[0].geometry.location, map, currentAddressInfo);
 
 	});
 
 }
 
-// Listen for submit on "find out more" button
-function findOutMore() {
-	$('.js-map-find-out-more').on('submit', event => {
-		event.preventDefault();
+function nyc311Call(event) {
+
+	event.preventDefault();
+
+	//The following code sets the date window for the AJAX call
+
+	let currentDate = new Date();
+	let subtract2Months = new Date();
+
+	console.log(currentDate);
+
+	console.log("Here is the current date's month");
+	console.log(currentDate.getMonth());
+
+	subtract2Months.setMonth(currentDate.getMonth() - 10);
+	console.log("Here is the current date's month minus 2");
+	console.log(subtract2Months.getMonth());
 
 
-	})
-}
+	let newDate = currentDate.toISOString().split("T")[0];
+	let refDate = subtract2Months.toISOString().split("T")[0];
 
-function Nyc311Call() {
+	console.log("Today's date:")
+	console.log(newDate);
+
+	//let refDate = newDate.setMonth(newDate.getMonth() - 2);
+
+	console.log("Reference Date:")
+	console.log(refDate);
+	//console.log(refDate);
+
+	let whenString = "created_date between '"+refDate+"' and '"+newDate+"'";
+	console.log("decoded string:");
+	console.log(whenString);
+
+
+	//the following code sets the lat long parameters
+
+	//LATITUDE
+
+	let latLngBoundDistance = 0.00449135414327;
+
+	let currentLat = currentAddressInfo.results[0].geometry.location.lat;
+	let currentLng = currentAddressInfo.results[0].geometry.location.lng;
+	console.log("here's the current location lat:");
+	console.log(currentLat);
+
+
+	let northLatBound = currentLat + latLngBoundDistance;
+	let southLatBound = currentLat - latLngBoundDistance;
+
+	let whereLatString = "latitude between '"+northLatBound+"' and '"+southLatBound+"'";
+
+	//LONGITUDE
+
+	console.log("here's the current location long:");
+	console.log(currentLng);
+
+	let westLngBound = currentLng - latLngBoundDistance;
+	let eastLngBound = currentLng + latLngBoundDistance;
+
 	
+
+	let whereLngString = "longitude between '"+westLngBound+"' and '"+eastLngBound+"'";
+
+
+	//what the URL should look like: https://data.cityofnewyork.us/resource/fhrw-4uyv.json?$where=created_date%20between%20%272017-07-16%27%20and%20%272017-12-17%27
+	const settings = {
+		url: NYC_311_API_ENDPOINT_URL,
+		data: {
+			"$$app_token": APP_TOKEN,
+			$query: `WHERE ${whenString} AND ${whereLatString} AND ${whereLngString}`
+			//created_date: refDate
+		},
+		type: "GET",
+		success: function(data) {
+			console.log("here's the 311 data!")
+			console.log(data);
+		}
+
+	};
+
+	$.ajax(settings);
+
 }
 
 
@@ -145,9 +222,11 @@ function renderNeighborhoodDataSideBar() {
 
 function main() {
 	listenForSearchInputThenRunFunctions();
+	//findOutMore();
 }
 
-main();
+$(main);
+
 
 
 
